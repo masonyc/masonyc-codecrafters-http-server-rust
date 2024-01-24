83a32dda18a7f5862aafb9107a13fcfa48ee5d86
masonyc <mason.yunchen@gmail.com>
2024-01-24 16:18:56 +1300

    feat: add response with 404

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,37 @@
-// Uncomment this block to pass the first stage
-// use std::net::TcpListener;
-
+use anyhow::{Context, Ok};
 use tokio::{
-    io::AsyncWriteExt,
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:4221").await?;
     loop {
         // The second item contains the IP and port of the new connection.
-        let (socket, _) = listener.accept().await?;
+        let (mut socket, _) = listener.accept().await?;
         tokio::spawn(async move {
-            process(socket).await;
+            if let Err(e) = process(&mut socket).await {
+                eprintln!("Error handling request {:?}", e)
+            };
         });
     }
 }
 
-async fn process(mut stream: TcpStream) {
-    let _ = stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").await;
+async fn process(stream: &mut TcpStream) -> anyhow::Result<()> {
+    let mut buf = [0u8; 1024];
+    stream
+        .read(&mut buf)
+        .await
+        .context("CTX: handle connection read buffer")?;
+    let data = String::from_utf8_lossy(&buf[..]);
+    let mut parts = data.split_whitespace();
+    let _ = parts.next();
+    let path = parts.next();
+    let response = match path {
+        Some("/") => "HTTP/1.1 200 OK\r\n\r\n",
+        _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+    };
+    stream.write_all(response.as_bytes()).await?;
+    Ok(())
 }
