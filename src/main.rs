1d5ce570c6f9eb35cea6d5292c867d08faaa2d16
masonyc <mason.yunchen@gmail.com>
2024-01-29 14:22:42 +1300

    feat: add read directory

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,37 @@
 use anyhow::{Context, Ok};
+use clap::Parser;
 use core::fmt;
-use std::collections::HashMap;
+use std::{collections::HashMap, fs::{self, File}, io::Read, path::Path, sync::Arc};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long)]
+    directory: String,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let arg = Arc::new(Args::parse());
     let listener = TcpListener::bind("127.0.0.1:4221").await?;
     loop {
         // The second item contains the IP and port of the new connection.
         let (mut socket, _) = listener.accept().await?;
+        let cloned_arg = arg.clone();
+
         tokio::spawn(async move {
-            if let Err(e) = process(&mut socket).await {
+            if let Err(e) = process(&mut socket, cloned_arg).await {
                 eprintln!("Error handling request {:?}", e)
             };
         });
     }
 }
 
-async fn process(stream: &mut TcpStream) -> anyhow::Result<()> {
+async fn process(stream: &mut TcpStream, arg: Arc<Args>) -> anyhow::Result<()> {
     let mut buf = [0u8; 1024];
     stream
         .read(&mut buf)
@@ -29,27 +40,49 @@ async fn process(stream: &mut TcpStream) -> anyhow::Result<()> {
 
     let request = HttpRequest::from_byte_array(&buf);
 
+    let mut headers = HashMap::new();
+
     let response = if request.verb == "GET" && request.path == "/" {
-        HttpResponse::new("".to_string(), request.protocol, 200)
+        HttpResponse::new("".to_string(), request.protocol, 200, headers)
     } else if request.verb == "GET" && request.path.starts_with("/echo/") {
         let echo_content = request
             .path
             .split_once("/echo/")
             .expect("Echo should contain content")
             .1;
-        HttpResponse::new(echo_content.to_string(), request.protocol, 200)
+        let response = HttpResponse::new(echo_content.to_string(), request.protocol, 200, headers);
+        response.prepare_plain_text_headers()
     } else if request.verb == "GET" && request.path == "/user-agent" {
         dbg!(
             "{:#?}",
             request.headers.get("User-Agent").unwrap().to_string()
         );
-        HttpResponse::new(
+        let response = HttpResponse::new(
             request.headers.get("User-Agent").unwrap().to_string(),
             request.protocol,
             200,
-        )
+            headers,
+        );
+        response.prepare_plain_text_headers()
+    } else if request.path.starts_with("/files/") {
+        let filename = request.path.split_once("/files").unwrap().1;
+        let path_str = format!("{}/{}", arg.directory, filename);
+        let path = Path::new(&path_str);
+
+        if path.exists() {
+            let body = fs::read_to_string(path).expect("Read file always sucuess");
+            let response= HttpResponse::new(
+                body,
+                request.protocol,
+                200,
+                headers
+            );
+            response.prepare_octet_stream_headers()
+        } else {
+            HttpResponse::new("".to_string(), request.protocol, 404, headers)
+        }
     } else {
-        HttpResponse::new("".to_string(), request.protocol, 404)
+        HttpResponse::new("".to_string(), request.protocol, 404, headers)
     };
     stream.write_all(response.to_string().as_bytes()).await?;
     Ok(())
@@ -59,10 +92,16 @@ struct HttpResponse {
     body: String,
     protocol: String,
     status: String,
+    headers: HashMap<String, String>,
 }
 
 impl HttpResponse {
-    fn new(body: String, protocol: String, status: u16) -> Self {
+    fn new(
+        body: String,
+        protocol: String,
+        status: u16,
+        headers: HashMap<String, String>,
+    ) -> Self {
         let status = if status == 200 {
             "200 OK"
         } else if status == 404 {
@@ -76,7 +115,24 @@ impl HttpResponse {
             body,
             protocol,
             status,
+            headers,
         }
+    }
+
+    fn prepare_plain_text_headers(mut self) -> HttpResponse {
+        self.headers
+            .insert("Content-Type".to_string(), "text/plain".to_string());
+        self.headers
+            .insert("Content-Length".to_string(), self.body.len().to_string());
+        self
+    }
+
+    fn prepare_octet_stream_headers(mut self) -> HttpResponse {
+        self.headers
+            .insert("Content-Type".to_string(), "application/octet-stream".to_string());
+        self.headers
+            .insert("Content-Length".to_string(), self.body.len().to_string());
+        self
     }
 }
 
